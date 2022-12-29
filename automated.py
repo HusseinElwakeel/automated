f98efb16e28f22194aa56c14ce20b60b6b628c86
HusseinElwakeel <71234180+HusseinElwakeel@users.noreply.github.com>
2022-12-29 14:28:53 +0200

    Delete automated.py

--- automated.py
+++
@@ -1,9 +0,0 @@
-import random
-import  pyautogui as pg
-import time
-# name =("A","B","C")
-time.sleep(8)                       #To stop 8 seconds
-for  i in  range(5000):             #to do what next 5000 time
-    # a=random.choice(name)         #if i want to send from choice
-    pg.write("Ya Hamoooooo")        #what will write
-    pg.press("enter")               #to press enter
