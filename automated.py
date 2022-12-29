d904a9cc0e53327afde20bbe2cad4b52430daa52
HusseinElwakeel <71234180+HusseinElwakeel@users.noreply.github.com>
2022-12-29 14:29:54 +0200

    Add files via upload

---
+++ automated.py
@@ -0,0 +1,9 @@
+import random
+import  pyautogui as pg
+import time
+# name =("A","B","C")
+time.sleep(8)                       #To stop 8 seconds
+for  i in  range(500):             #to do what next 5000 time
+    # a=random.choice(name)         #if i want to send from choice
+    pg.write("Ya Hamoooooo")        #what you need to write
+    pg.press("enter")               #to press enter
